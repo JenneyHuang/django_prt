90f385ad5e49a080c75f41ea9a89649c42d25cad
JenneyHuang <hlphyx@gmail.com>
2018-11-16 00:57:22 +0800

    third commit

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,11 @@ from django.conf.urls import url,include
 import blog.views
 
 urlpatterns = [
-    url(r'^$',blog.views.archive),
+    url(r'^$',blog.views.archive,name='blogpost'),
+    url(r'^post/(?P<pk>[0-9]+)/$', blog.views.post_detail, name='post_detail'),
+    url(r'^post/new/$', blog.views.post_new, name='post_new'),
+
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', blog.views.post_edit, name='post_edit'),
     url(r'^create/', blog.views.create_blogpost),
-    url(r'^table/',blog.views.table),
+    #url(r'^table/',blog.views.table),
 ]
--- blog/views.py
+++ blog/views.py
@@ -10,18 +10,19 @@ from blog.models import Employee
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from blog.models import BlogPostForm
-
-
+from django.shortcuts import redirect
+from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
 @csrf_protect
 def archive(request):
     # post = BlogPost(title='mocktitle', body = 'mockbody',
     #                 timestamp = datetime.now())
     posts = BlogPost.objects.all()
-    return render(request,'index.html',{'posts':posts,'form':BlogPostForm },RequestContext(request))
+    return render(request, 'blog/index.html', {'posts':posts, 'form':BlogPostForm}, RequestContext(request))
 
 def table(request):
     names = Employee.objects.all()
-    return render_to_response('table.html',{'names':names})
+    return render_to_response('blog/templates/table.html', {'names':names})
 
 def create_blogpost(request):
     if request.method == 'POST':
@@ -40,3 +41,32 @@ def create_blogpost(request):
             form.save()
     return HttpResponseRedirect('/blog/')
 
+def post_detail(request, pk):
+    post = get_object_or_404(BlogPost, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    form = PostForm(request.POST)
+    if form.is_valid():
+        post = form.save(commit = False)
+        post.author = request.user
+        post.timestamp = datetime.now()
+        post.save()
+        return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request,pk):
+    post = get_object_or_404(BlogPost, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.timestamp = datetime.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
